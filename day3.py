c8ca15545bcb6575498919f5f5e2060e21b32dc0
Jason Hussey <jphussey05@gmail.com>
2019-12-03 22:20:33 -0500

    day3 p1 done

---
+++ day3.py
@@ -0,0 +1,57 @@
+from shapely.geometry import LineString
+
+def decompose(figure):
+    moves = figure.split(',')
+    points = []
+    
+    for move in moves:
+        direction = move[0]
+        length = int(move[1:])
+        if points:
+            prev_point = newpoint
+        else:
+            prev_point = (0,0)
+        prev_x, prev_y = prev_point
+
+        if direction == 'R':
+            newpoint = prev_x + length, prev_y
+        elif direction == 'L':
+            newpoint = prev_x - length, prev_y
+        elif direction == 'D':
+            newpoint = prev_x, prev_y - length
+        elif direction == 'U':
+            newpoint = prev_x, prev_y + length
+        else:
+            print(f'Encountered an incorrect direction, {direction}, bailing!')
+            raise SystemExit
+
+        points.append(LineString([prev_point, newpoint]))
+
+    return points
+
+if __name__ == "__main__":
+    
+    with open('day3.txt') as fin:
+        contents = fin.readlines()
+
+    fig1, fig2 = contents[0].strip(), contents[1].strip()
+    isections = list()
+
+    lines1 = decompose(fig1)
+    lines2 = decompose(fig2)    
+
+    for line_a in lines1:
+        for line_b in lines2:
+            result = line_a.intersection(line_b)
+            if result:
+                isections.append(result)
+
+    min_length = None
+    
+    for i in isections[1:]:
+        man_dist = abs(i.bounds[0]) + abs(i.bounds[1])
+        if not min_length or man_dist < min_length:
+            min_length = man_dist
+            min_point = i
+
+    print(f'Minimum manhattan distance is {min_length} to {min_point}')
