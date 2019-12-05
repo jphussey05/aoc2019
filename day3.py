a9c14a5d2a84008318efa971b3d43fa8349d05ca
Jason Hussey <jphussey05@gmail.com>
2019-12-04 21:10:27 -0500

    day3 p2 complete. better use of shapely

--- day3.py
+++ day3.py
@@ -1,4 +1,6 @@
 from shapely.geometry import LineString
+from shapely.ops import split
+from shapely.geometry import Point
 
 def decompose(figure):
     moves = figure.split(',')
@@ -31,29 +33,37 @@ def decompose(figure):
 
 if __name__ == "__main__":
     
+    # read in instructions
     with open('day3.txt') as fin:
         contents = fin.readlines()
+        fig1, fig2 = contents[0].strip(), contents[1].strip()
 
-    fig1, fig2 = contents[0].strip(), contents[1].strip()
-    isections = list()
-
+    # create LineFigures from instructions and then find intersections
     lines1 = decompose(fig1)
     lines2 = decompose(fig2)    
-
-    
     isections = lines1.intersection(lines2)
 
     min_length = None
-    
+    min_steps = None
     for i in isections:
+        if i == Point((0,0)):  # guarding clause for origin point
+            continue
+
         man_dist = abs(i.bounds[0]) + abs(i.bounds[1])
-        print(f'Intersection at {i}, distance is {man_dist}')
+        
+        # split figures at each intersection and total length of first halves 
+        partial_line1 = split(lines1, i)
+        partial_line2 = split(lines2, i)
+        steps = partial_line1[0].length + partial_line2[0].length
 
         if not min_length or man_dist < min_length:
             min_length = man_dist
             min_point = i
+        if not min_steps or steps < min_steps:
+            min_steps = steps
+            min_step_point = i
 
+        print(f'{i} - Manhattan distance is {man_dist}, Steps are {steps}')
 
-    print(f'Minimum manhattan distance is {min_length} to {min_point}')
-
-    # TODO take all of the intersections and step through the decomposed line strings calculating the length all the way. Find minimum.
+    print(f'Minimum Manhattan distance is {min_length} to {min_point}')
+    print(f'Minimum steps to an intersection are {min_steps} to {min_step_point}')
